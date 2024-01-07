6b041db98c70f1852edf71e64e8cf5b9a0325bd9
Muhammad <muhammadahjahh@gmail.com>
2024-01-07 11:11:59 +0800

    pppl taro sini

---
+++ PPPL/Translate-Java/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Translate_Java
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnbrowse = new System.Windows.Forms.Button();
+            this.btnexport = new System.Windows.Forms.Button();
+            this.btngenerate = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnhelp = new System.Windows.Forms.Button();
+            this.btnparsing = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnbrowse
+            // 
+            this.btnbrowse.AutoSize = true;
+            this.btnbrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnbrowse.Location = new System.Drawing.Point(40, 86);
+            this.btnbrowse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnbrowse.Name = "btnbrowse";
+            this.btnbrowse.Size = new System.Drawing.Size(92, 34);
+            this.btnbrowse.TabIndex = 0;
+            this.btnbrowse.Text = "Browse";
+            this.btnbrowse.UseVisualStyleBackColor = true;
+            this.btnbrowse.Click += new System.EventHandler(this.btnbrowse_Click_1);
+            // 
+            // btnexport
+            // 
+            this.btnexport.AutoSize = true;
+            this.btnexport.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexport.Location = new System.Drawing.Point(40, 316);
+            this.btnexport.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnexport.Name = "btnexport";
+            this.btnexport.Size = new System.Drawing.Size(77, 33);
+            this.btnexport.TabIndex = 1;
+            this.btnexport.Text = "Export";
+            this.btnexport.UseVisualStyleBackColor = true;
+            this.btnexport.Click += new System.EventHandler(this.btnexport_Click_1);
+            // 
+            // btngenerate
+            // 
+            this.btngenerate.AutoSize = true;
+            this.btngenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngenerate.Location = new System.Drawing.Point(40, 146);
+            this.btngenerate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btngenerate.Name = "btngenerate";
+            this.btngenerate.Size = new System.Drawing.Size(105, 34);
+            this.btngenerate.TabIndex = 2;
+            this.btngenerate.Text = "Generate";
+            this.btngenerate.UseVisualStyleBackColor = true;
+            this.btngenerate.Click += new System.EventHandler(this.btngenerate_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(201, 86);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(600, 544);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(809, 86);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(600, 544);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(235, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "JSON Model";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1079, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Java";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnclear
+            // 
+            this.btnclear.AutoSize = true;
+            this.btnclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclear.Location = new System.Drawing.Point(40, 379);
+            this.btnclear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(117, 33);
+            this.btnclear.TabIndex = 7;
+            this.btnclear.Text = "Clear Code";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnhelp
+            // 
+            this.btnhelp.AutoSize = true;
+            this.btnhelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnhelp.Location = new System.Drawing.Point(40, 260);
+            this.btnhelp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnhelp.Name = "btnhelp";
+            this.btnhelp.Size = new System.Drawing.Size(75, 33);
+            this.btnhelp.TabIndex = 8;
+            this.btnhelp.Text = "Help";
+            this.btnhelp.UseVisualStyleBackColor = true;
+            this.btnhelp.Click += new System.EventHandler(this.btnhelp_Click);
+            // 
+            // btnparsing
+            // 
+            this.btnparsing.AutoSize = true;
+            this.btnparsing.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnparsing.Location = new System.Drawing.Point(40, 204);
+            this.btnparsing.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnparsing.Name = "btnparsing";
+            this.btnparsing.Size = new System.Drawing.Size(105, 34);
+            this.btnparsing.TabIndex = 9;
+            this.btnparsing.Text = "Parsing";
+            this.btnparsing.UseVisualStyleBackColor = true;
+            this.btnparsing.Click += new System.EventHandler(this.btnparsing_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1442, 697);
+            this.Controls.Add(this.btnparsing);
+            this.Controls.Add(this.btnhelp);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btngenerate);
+            this.Controls.Add(this.btnexport);
+            this.Controls.Add(this.btnbrowse);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "GenerateJava";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnbrowse;
+        private System.Windows.Forms.Button btnexport;
+        private System.Windows.Forms.Button btngenerate;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnhelp;
+        private System.Windows.Forms.Button btnparsing;
+    }
+}
+
---
+++ PPPL/Translator/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.AxHost;
+
+namespace xtUML1
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private string selectedFilePath;
+        private bool isJsonFileSelected = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog = new OpenFileDialog();
+            saveFileDialog = new SaveFileDialog();
+        }
+
+        private void btnSelect_Click_1(object sender, EventArgs e)
+        {
+            // Menampilkan dialog untuk memilih file JSON
+            openFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+            openFileDialog.Title = "Select JSON File";
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+
+                // ubah semua sesuai kebutuhan
+
+                string jsonContent = File.ReadAllText(openFileDialog.FileName);
+
+                dynamic jsonObj = JObject.Parse(jsonContent);
+
+                selectedFilePath = openFileDialog.FileName;
+
+                textBox1.Text = selectedFilePath;
+
+                // tampilkan sintax error parsing di textBox4 jika tidak lolos parsing
+
+                textBox4.Text = jsonContent; // untuk menampilkan isi file json (setelah lolos parsing)
+
+                isJsonFileSelected = true;
+            }
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            // parsing
+            // tampilkan sintax error parsing di textBox4 jika tidak lolos parsing
+            // tampilkan isi file json di textBox4 jika lolos parsing
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            // translate jika lolos parsing
+            // tampilkan hasil translate di textBox3
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // tulis method untuk menghapus nilai textBox1 (selected file)
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            string helpMessage = OpenHelp(); // tulis isi help di sini
+            MessageBox.Show(helpMessage, "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private string OpenHelp()
+        {
+            StringBuilder helpMessage = new StringBuilder();
+
+            helpMessage.AppendLine("User Guide for Generating JSON Model into Java Programming Language");
+            helpMessage.AppendLine();
+            helpMessage.AppendLine("1. Open the desired JSON File by clicking Browse button");
+            helpMessage.AppendLine("2. After the code appeared in the box, click the Generate button to translate it to Java");
+            helpMessage.AppendLine("3. The Java Code will exist in the right box and click Export button to save it as Java file");
+
+            return helpMessage.ToString();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (textBox3.TextLength > 0)
+            {
+                textBox3.SelectAll();
+                textBox3.Copy();
+                MessageBox.Show("Successfully Copied!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Please Translate First!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!isJsonFileSelected)
+            {
+                MessageBox.Show("Select JSON file as an input first!", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (textBox3.TextLength > 0)
+            {
+                saveFileDialog.Filter = "C++ files (*.cpp)|*.cpp|All files (*.*)|*.*"; // ubah ekstensi output save file
+                saveFileDialog.Title = "Save C++ File"; // ubah C++
+
+                DialogResult result = saveFileDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string cppCode = textBox3.Text;
+
+                    File.WriteAllText(saveFileDialog.FileName, cppCode);
+
+                    selectedFilePath = saveFileDialog.FileName;
+
+                    MessageBox.Show("Successfully Saved!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Translate First!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+
+        private void btnVisualize_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("We are sorry, this feature is not available right now.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return;
+        }
+
+        private void btnSimulate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("We are sorry, this feature is not available right now.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return;
+        }
+    }
+}
